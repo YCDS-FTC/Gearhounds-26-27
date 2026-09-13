d66ce58ce37b0af283ad6856b831359a3395e7bb
Gearhounds9242 <ycds9242@gmail.com>
2026-09-12 22:07:53 -0400

    addedPoses for hive but might be bad-waiting for pedro visualizer to be finalized.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Fieldconstants.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.utilities;
+
+public class Fieldconstants {
+    /*Hive Positions:
+    Red Hive red loading zone side (tilted up position, BACK RIGHT CORNER):
+    Pose 53.23608017817369, 48.170378619153674
+
+    Red Hive red loading zone side (tilted up position, BACK LEFT CORNER):
+    Pose 53.23608017817369, 68.65478841870822
+
+    Red Hive red garden side(tilted up position):
+    Pose
+     */
+
+}
